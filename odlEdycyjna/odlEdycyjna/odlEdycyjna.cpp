fad6a24f075d72d6a74fa9ce7c2d8ddfb3e3425d
rudzia <alicjastrak94@gmail.com>
2017-05-15 19:53:48 +0200

    Giaro nowe macierze

--- odlEdycyjna/odlEdycyjna/odlEdycyjna.cpp
+++ odlEdycyjna/odlEdycyjna/odlEdycyjna.cpp
@@ -4,190 +4,271 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-
+#include <algorithm>
+#include <fstream>
 using namespace std;
+
+int similarityMetric[5][5]= { { 1, -1, -1, -1, -1 },
+							  { -1, 1, -1, -1, -1 },
+							  { -1, -1, 1, -1, -1 },
+							  { -1, -1, -1, 1, -1 },
+							  { -1, -1, -1, -1, 0 } };
+
+int distanceMetric[5][5] = {{ 0, 1, 1, 1, 1 },
+							{ 1, 0, 1, 1, 1 },
+							{ 1, 1, 0, 1, 1 },
+							{ 1, 1, 1, 0, 1 },
+							{ 1, 1, 1, 1, 0 } };
+
+//bool controlWord(string word)
 
 struct node
 {
 	int value;
 	int previousCell;
 };
-
-int min(int ij_1, int i_1j_1, int i_1j)
+int d(char letterFroWord1, char letterFromWord2)
 {
-	if (ij_1 <= i_1j_1 && ij_1 <= i_1j )
+	char alphabet[5] = { 'A', 'C', 'T', 'G', '-' };
+	int i, j;
+
+	switch(letterFroWord1)
 	{
-		return ij_1;
+		case 'A': i = 0; break;
+		case 'C': i = 1; break;
+		case 'T': i = 2; break;
+		case 'G': i = 3; break;
+		case '-': i = 4; break;
 	}
-	else
+	switch (letterFromWord2)
 	{
-		if (i_1j_1 <= ij_1 && i_1j_1 <= i_1j)
-			return i_1j_1;
-		else
-			return i_1j;
+		case 'A': j = 0; break;
+		case 'C': j = 1; break;
+		case 'T': j = 2; break;
+		case 'G': j = 3; break;
+		case '-': j = 4; break;
 	}
+
+	return distanceMetric[i][j];
 }
-int max(int ij_1, int i_1j_1, int i_1j, int &selected)
+int s(char letterFroWord1, char letterFromWord2)
 {
-	if (i_1j_1 >= ij_1 && i_1j_1 >= i_1j)
+	char alphabet[5] = { 'A', 'C', 'T', 'G', '-' };
+	int i, j;
+
+	switch (letterFroWord1)
 	{
-		selected = 2;
-		return i_1j_1;		
+		case 'A': i = 0; break;
+		case 'C': i = 1; break;
+		case 'T': i = 2; break;
+		case 'G': i = 3; break;
+		case '-': i = 4; break;
 	}
-	else
+	switch (letterFromWord2)
 	{
-		if(ij_1 >= i_1j_1 && ij_1 >= i_1j)
-		{
-			selected = 1;
-			return ij_1;
-		}
-		else
-		{
-			selected = 3;
-			return i_1j;
-		}
+		case 'A': j = 0; break;
+		case 'C': j = 1; break;
+		case 'T': j = 2; break;
+		case 'G': j = 3; break;
+		case '-': j = 4; break;
 	}
+
+	return similarityMetric[i][j];
 }
-
-void odlegloscEdycyjna(string word1, string word2)
+void wypiszWynikDopasowania(node **ArrayMatching, int rows, int column, string word1, string word2)
 {
-	int column, rows;
-	int sameLetter;
-	rows = word1.length() + 1;
-	column = word2.length() + 1;
-
-	node ** arrayDistance = new node *[rows];
-	for (int i = 0; i < rows; i++)
-	{
-		arrayDistance[i] = new node[column];
-	}
-
-	for (int n = 0; n < column; n++)
-	{
-		arrayDistance[0][n].value = n;
-	}
-	for (int m = 0; m < rows; m++)
-	{
-		arrayDistance[m][0].value = m;
-	}
-
-	for (int i = 1; i < rows; i++)
-	{
-		for (int j = 1; j < column; j++)
-		{
-			if (word1[i - 1] == word2[j - 1])
-				sameLetter = 0;
-			else
-				sameLetter = 1;
-
-			arrayDistance[i][j].value = min((arrayDistance[i][j - 1].value + 1), 
-				                            (arrayDistance[i - 1][j - 1].value + sameLetter), 
-				                            (arrayDistance[i - 1][j].value + 1));
-		}
-	}
-
-	cout << "Odleglosc edycyjna wynosi: " << arrayDistance[rows - 1][column - 1].value << endl;
-
-	for (int i(0); i < rows; ++i)
-		delete[] arrayDistance[i]; 
-	delete[] arrayDistance; 
-}
-
-void podobienstwoSekwencji(string word1, string word2)
-{
-	int column, rows;
-	int sameLetter;
-	int selectedCell = 0;
-	column = word1.length() + 1;
-	rows = word2.length() + 1;
-
-	node ** sequenceSimilarity = new node *[rows];
-	for (int i = 0; i < rows; i++)
-	{
-		sequenceSimilarity[i] = new node[column];
-	}
-	for (int n = 0; n < column; n++)
-	{
-		sequenceSimilarity[0][n].value = 0;
-	}
-	for (int m = 0; m < rows; m++)
-	{
-		sequenceSimilarity[m][0].value = 0;
-	}
-	for (int i = 1; i < rows; i++)
-	{
-		for (int j = 1; j < column; j++)
-		{
-			if (word2[i - 1] == word1[j - 1])
-				sameLetter = 1;
-			else
-				sameLetter = 0;
-
-			sequenceSimilarity[i][j].value = max((sequenceSimilarity[i][j - 1].value),
-				                                 (sequenceSimilarity[i - 1][j - 1].value + sameLetter),
-				                                 (sequenceSimilarity[i - 1][j].value),
-												  selectedCell);
-			sequenceSimilarity[i][j].previousCell = selectedCell;
-
-			selectedCell = 0;
-		}
-	}
-
-	cout << "Podobienstwo sekwencji wynosi: " << sequenceSimilarity[rows - 1][column - 1].value << endl;
-	getchar();
-
 	string newWord1, newWord2;
 	int y = rows - 1;
 	int x = column - 1;
 
 	for (int i = 1; i < rows + column; i++)
 	{
-		if (sequenceSimilarity[y][x].previousCell == 2)
+		if (ArrayMatching[y][x].previousCell == 2)
 		{
-			newWord1.insert(0,1,word1[x -1]);
-			newWord2.insert(0,1,word2[y - 1]);
+			newWord1.insert(0, 1, word1[x - 1]);
+			newWord2.insert(0, 1, word2[y - 1]);
 			x--;
 			y--;
 		}
 		else
-			if (sequenceSimilarity[y][x].previousCell == 3)
+			if (ArrayMatching[y][x].previousCell == 3)
 			{
-				newWord1.insert(0,1, '-');
-				newWord2.insert(0,1,word2[y-1]);
+				newWord1.insert(0, 1, '-');
+				newWord2.insert(0, 1, word2[y - 1]);
 				y--;
 			}
 			else
 			{
-				newWord1.insert(0,1,word1[x - 1]);
-				newWord2.insert(0,1,'-');
+				newWord1.insert(0, 1, word1[x - 1]);
+				newWord2.insert(0, 1, '-');
 				x--;
 			}
-		if (x - 1 < 0 || y-1 < 0)
+		if (x==0 && y==0 || x < 0 || y < 0)
 		{
 			break;
 		}
 	}
-		cout << newWord1 << endl;
-		cout << newWord2 << endl;
-	
-	for (int i =0; i < rows; ++i)
-		delete[] sequenceSimilarity[i];
-	delete[] sequenceSimilarity;
+
+	cout << newWord1 << endl;
+	cout << newWord2 << endl;	
 }
 
+int min(int ij_1, int i_1j_1, int i_1j, int &selectedCell)
+{
+	if (i_1j_1 < ij_1 && i_1j_1 <= i_1j)
+	{
+		selectedCell = 2;
+		return i_1j_1;
+	}
+	else
+	{
+		if (ij_1 <= i_1j_1 && ij_1 <= i_1j)
+		{
+			selectedCell = 1;
+			return ij_1;
+		}
+		else
+		{
+			selectedCell = 3;
+			return i_1j;
+		}
+	}
+}
+int max(int ij_1, int i_1j_1, int i_1j, int &selectedCell) // selected cell: 1 - left, 2 - slant, 3 - up
+{
+	if (i_1j_1 >= ij_1 && i_1j_1 >= i_1j)
+	{
+		selectedCell = 2;
+		return i_1j_1;		
+	}
+	else
+	{
+		if(ij_1 >= i_1j_1 && ij_1 >= i_1j)
+		{
+			selectedCell = 1;
+			return ij_1;
+		}
+		else
+		{
+			selectedCell = 3;
+			return i_1j;
+		}
+	}
+}
+
+void calculateEditDistance(string word1, string word2)
+{
+	int column, rows;
+	int selectedCell = 0;
+	column = word1.length() + 1;
+	rows = word2.length() + 1;
+
+	node ** distanceArray = new node *[rows];
+	for (int i = 0; i < rows; i++)
+	{
+		distanceArray[i] = new node[column];
+	}
+	for (int n = 0; n < column; n++)
+	{
+		distanceArray[0][n].value = n;
+		distanceArray[0][n].previousCell = 1;
+	}
+	for (int m = 0; m < rows; m++)
+	{
+		distanceArray[m][0].value = m;
+		distanceArray[m][0].previousCell = 3;
+	}
+	for (int i = 1; i < rows; i++)
+	{
+		for (int j = 1; j < column; j++)
+		{
+			distanceArray[i][j].value = min((distanceArray[i][j - 1].value + d('-',word2[i-1])), 
+				                            (distanceArray[i - 1][j - 1].value + d(word1[j-1],word2[i-1])), 
+				                            (distanceArray[i - 1][j].value + d(word1[j-1],'-')), selectedCell);
+
+			distanceArray[i][j].previousCell = selectedCell;
+			selectedCell = 0;
+		}
+	}
+
+	cout << "Odleglosc edycyjna wynosi: " << distanceArray[rows - 1][column - 1].value << '\n'<<endl;
+
+	wypiszWynikDopasowania(distanceArray, rows, column, word1, word2);
+
+
+	for (int i(0); i < rows; ++i)
+		delete[] distanceArray[i]; 
+	delete[] distanceArray; 
+}
+
+void similarityWords(string word1, string word2)
+{
+	int column, rows;
+	int isSameLetter;
+	int selectedCell = 0;
+	column = word1.length() + 1;
+	rows = word2.length() + 1;
+
+	node ** sequenceSimilarityArray = new node *[rows];
+	for (int i = 0; i < rows; i++)
+	{
+		sequenceSimilarityArray[i] = new node[column];
+	}
+	for (int n = 0; n < column; n++)
+	{
+		sequenceSimilarityArray[0][n].value = 0;
+		sequenceSimilarityArray[0][n].previousCell = 1;
+	}
+	for (int m = 0; m < rows; m++)
+	{
+		sequenceSimilarityArray[m][0].value = 0;
+		sequenceSimilarityArray[m][0].previousCell = 3;
+	}
+	for (int i = 1; i < rows; i++)
+	{
+		for (int j = 1; j < column; j++)
+		{
+			sequenceSimilarityArray[i][j].value = max((sequenceSimilarityArray[i][j - 1].value + s('-', word2[i - 1])),
+													  (sequenceSimilarityArray[i - 1][j - 1].value + s(word1[j - 1], word2[i - 1])),
+													  (sequenceSimilarityArray[i - 1][j].value + s(word1[j - 1], '-')),
+													   selectedCell);
+												  
+			sequenceSimilarityArray[i][j].previousCell = selectedCell;
+			selectedCell = 0;
+		}
+	}
+
+	string word11 = word1;
+	string word22 = word2;
+	cout << "Podobienstwo sekwencji wynosi: " << sequenceSimilarityArray[rows - 1][column - 1].value << '\n'<< endl;
+
+	wypiszWynikDopasowania(sequenceSimilarityArray, rows, column, word1, word2);
+
+	for (int i =0; i < rows; ++i)
+		delete[] sequenceSimilarityArray[i];
+	delete[] sequenceSimilarityArray;
+}
 
 int main()
 {
 	string word1;
 	string word2;
-	cout << "Podaj pierwszy wyraz: " << endl;
-	cin >> word1;
-	cout << "Podaj drugi wyraz: " << endl;
-	cin >> word2;
-	//zrobic potem wczytywanie z pliku
-	odlegloscEdycyjna(word1, word2);
-	podobienstwoSekwencji(word1, word2);
 
+	fstream txtFile;
+	txtFile.open("sequences.txt", ios::in | ios::out);
+	if (txtFile.is_open())
+	{
+		cout << "Wczytane dwie sekwencje:" << endl;
+		getline(txtFile, word1);
+		cout << word1 << endl;
+		getline(txtFile, word2);
+		cout << word2 << endl;
+		txtFile.close();
+	}
+
+	cout << '\n' << endl;
+	calculateEditDistance(word1, word2);
+	similarityWords(word1, word2);
 
     return 0;
 }
